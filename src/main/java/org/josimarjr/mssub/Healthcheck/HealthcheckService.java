2d96b87b53f70706f0e10977fca49f9664450db1
josimar-jr <shikamarujunior@gmail.com>
2020-08-23 14:45:07 -0300

    altera a resposta para a string adequada de json

--- src/main/java/org/josimarjr/mssub/Healthcheck/HealthcheckService.java
+++ src/main/java/org/josimarjr/mssub/Healthcheck/HealthcheckService.java
@@ -5,6 +5,7 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class HealthcheckService {
     public String Ok() {
-        return "{'status': 'ok'}";
+        String status = "{\"status\": \"ok\"}";
+        return status;
     }
 }
