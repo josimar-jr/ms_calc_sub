3813b4a837dbe085f79132391cd43c58933cc924
josimar-jr <shikamarujunior@gmail.com>
2020-09-05 12:02:40 -0300

    adiciona testes que cobrem as situações com números negativos

--- src/test/java/org/josimarjr/mssub/Subtraction/SubtractionServiceTest.java
+++ src/test/java/org/josimarjr/mssub/Subtraction/SubtractionServiceTest.java
@@ -29,4 +29,44 @@ public class SubtractionServiceTest {
         double result = subtraction.Subtracts(x, y);
         assertEquals(11.88, result, 0.001);
     }
+
+    @Test
+    public void subtractTwoIntegersWithNegativeResult() {
+        SubtractionService subtraction = new SubtractionService();
+        int x = 3;
+        int y = 13;
+
+        int result = subtraction.Subtracts(x, y);
+        assertEquals(-10, result);
+    }
+
+    @Test
+    public void subtractTwoDecimalsWithNegativeResult() {
+        SubtractionService subtraction = new SubtractionService();
+        double x = 7.01;
+        double y = 10.13;
+
+        double result = subtraction.Subtracts(x, y);
+        assertEquals(-3.12, result, 0.001);
+    }
+
+    @Test
+    public void subtractTwoNegativeIntegers() {
+        SubtractionService subtraction = new SubtractionService();
+        int x = -3;
+        int y = 13;
+
+        int result = subtraction.Subtracts(x, y);
+        assertEquals(-16, result);
+    }
+
+    @Test
+    public void subtractTwoNegativeDecimals() {
+        SubtractionService subtraction = new SubtractionService();
+        double x = -7.01;
+        double y = 10.13;
+
+        double result = subtraction.Subtracts(x, y);
+        assertEquals(-17.14, result, 0.001);
+    }
 }
