08fe15df525391d937cd6aff3563c0920c681bcf
josimar-jr <shikamarujunior@gmail.com>
2020-09-05 11:46:39 -0300

    adiciona teste base para o serviço de subtração

--- src/test/java/org/josimarjr/mssub/Subtraction/SubtractionServiceTest.java
+++ src/test/java/org/josimarjr/mssub/Subtraction/SubtractionServiceTest.java
@@ -1,2 +1,22 @@
-package org.josimarjr.mssub.Subtraction;public class SubtractionServiceTest {
+package org.josimarjr.mssub.Subtraction;
+
+import org.josimarjr.mssub.Healthcheck.HealthcheckService;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Tag("unit-test")
+public class SubtractionServiceTest {
+
+    @Test
+    public void subtractTwoIntegers() {
+        SubtractionService subtraction = new SubtractionService();
+        int x = 13;
+        int y = 1;
+
+        int result = subtraction.Subtracts(x, y);
+        assertEquals(12, result);
+    }
 }
